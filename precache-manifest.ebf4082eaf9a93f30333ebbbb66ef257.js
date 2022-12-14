56cf4c09d4f45cbf6fcdcdb1e8e91d1b917cebd7
mohamed magdy ismael <mohamed.magdy@vezeeta.com>
2022-12-14 18:16:53 +0200

    Updates

---
+++ precache-manifest.ebf4082eaf9a93f30333ebbbb66ef257.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a51791880850b146e60fa3bec1f421",
+    "url": "/Scheduler/index.html"
+  },
+  {
+    "revision": "3072c8749302f5f602e2",
+    "url": "/Scheduler/static/css/main.dc321410.chunk.css"
+  },
+  {
+    "revision": "08ee72156714ca26cb26",
+    "url": "/Scheduler/static/js/2.2f26ef61.chunk.js"
+  },
+  {
+    "revision": "3072c8749302f5f602e2",
+    "url": "/Scheduler/static/js/main.d3994d07.chunk.js"
+  },
+  {
+    "revision": "1f9f8589381f150dad7d",
+    "url": "/Scheduler/static/js/runtime-main.13023992.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Calendar/precache-manifest.9a3d4ea6cf66f3c73eff1c2eec213428.js"
+  "/Scheduler/precache-manifest.ebf4082eaf9a93f30333ebbbb66ef257.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Calendar/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Scheduler/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
